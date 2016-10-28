8f797db00c9f831125aa8bd09dfb446765319077
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2016-10-27 20:16:54 -0700

    Able to see messages from other users, not mines

--- Chat-client/ChatViewController.swift
+++ Chat-client/ChatViewController.swift
@@ -7,13 +7,41 @@
 //
 
 import UIKit
+import Parse
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messages: [PFObject]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let query = PFQuery(className:"Message")
+        query.findObjectsInBackground {
+            (objects, error) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) scores.")
+                
+                self.messages = objects
+                self.tableView.reloadData()
+                
+                if let objects = objects {
+                    for object in objects {
+                        print(object.objectId)
+                    }
+                }
+            } else {
+                // Log details of the failure
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +49,37 @@ class ChatViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSendMessageButton(_ sender: AnyObject) {
+        let newMessage = messageField.text
+        
+        let message = PFObject(className: "Messages")
+        message["text"] = newMessage
+        
+        PFObject.saveAll(inBackground: [message]) { (succeeded, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print(succeeded)
+            }
+        }
     }
-    */
+}
 
+extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if messages != nil {
+            return messages.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath)
+
+        cell.textLabel?.text = messages[indexPath.row]["text"] as! String?
+        
+        return cell
+    }
+    
 }
